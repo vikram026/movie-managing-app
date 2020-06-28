d31ab6f540fba0aebef8ba6f52da0ee850a79bf2
Vikram <Vikram.Singh@albertsons.com>
2020-06-28 21:18:19 +0530

    initial commit

---
+++ src/main/java/com/kommunicate/MovieManagingAppApplication.java
@@ -0,0 +1,13 @@
+package com.kommunicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kommunicate/MovieManagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kommunicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
